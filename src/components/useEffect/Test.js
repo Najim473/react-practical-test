746306b2ecec20d5fb303126d33d16402715676a
Najim Uddin <najim7304@gmail.com>
2023-05-10 12:58:09 +0600

    added dependency array

--- src/components/useEffect/Test.js
+++ src/components/useEffect/Test.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
 
 const Test = () => {
+    const [count, setCount] = useState(0);
+    const [time, setTime] = useState(new Date());
+    // TIME WILL CHANGED AUTOMATICALLY 
+    // useEffect(() => {
+    //     setTime(new Date())
+    // })
+    // TIME TO COUNTING WILL BE CHANGED 
+    useEffect(() => {
+        setTime(new Date())
+    }, [count])
     return (
-        <div>Test</div>
+        <div>
+            <button onClick={() => setCount((count) => count + 1)}>Count Time</button>
+            <p>{time.toLocaleString()}</p>
+        </div>
     )
 }
 
